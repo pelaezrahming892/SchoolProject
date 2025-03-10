b7ec72f89f1871c202ce8f9c30406290af2849e7
pelaezrahming892 <pelaezrahming892@gmail.com>
2025-03-10 00:00:09 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    int sum = num1 + num2;
+    printf("The sum is %d\n", sum);
+    return 0;
+}
