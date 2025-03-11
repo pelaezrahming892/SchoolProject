468d3b871981c2d2ebc2eb86e3a0ca7b0e3b3a16
pelaezrahming892 <pelaezrahming892@gmail.com>
2025-03-11 08:20:56 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int number = 5;
+    double result = sqrt(number);
+    printf("The square root of %d is %f\n", number, result);
+    return 0;
+}
