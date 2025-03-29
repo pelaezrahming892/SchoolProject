6033c1d5b5f6387ebfa7629d0246ca0743183f61
pelaezrahming892 <pelaezrahming892@gmail.com>
2025-03-29 20:06:33 +0000

    Add 15.c

---
+++ src/15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    int y = 3;
+
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("y is greater than x\n");
+    }
+
+    return 0;
+}
